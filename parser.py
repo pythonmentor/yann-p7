d37199b1de614aa47fc375d7abb5e9438e71034f
YANN HAMDI <hamdiyann@gmail.com>
2019-02-07 16:57:10 +0100

    test pour passer au vert

--- parser.py
+++ parser.py
@@ -1,8 +1,8 @@
 """docs function sentence parsing"""
-
-def in_lower_case(self, sentence):
-	self.sentence = ""
-	return self.sentence.lower()
+Class Sentence_parse():
+    def in_lower_case(self, sentence):
+	    self.sentence = str(sentence)   
+	    return self.sentence.lower()
 #METTRE TOUT EN MINUSCULE
 #SUPPRIMER LES STOPS WORDS
 #RETOURNER PHRASE NETTOYEE
