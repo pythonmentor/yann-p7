5ce688f8533281370c8a832962e031cdc1e9c03a
YANN HAMDI <hamdiyann@gmail.com>
2019-02-08 16:47:24 +0100

    test pour supprimer les stop words

--- parser.py
+++ parser.py
@@ -2,9 +2,22 @@
 class SentenceParse:
         
     def in_lower_case(self, sentence):
-	    self.sentence = str(sentence)   
-	    return(self.sentence.lower())
+        self.sentence = str(sentence)   
+        return(self.sentence.lower())
+        
 
+
+
+
+def main():
+    parse= SentenceParse()
+    x= "JE ne suis pas beau"
+    parse.in_lower_case(x)
+    for word in parse.sentence:
+        print(word)
+main()
+    
+    
 
 #SUPPRIMER LES STOPS WORDS
 #RETOURNER PHRASE NETTOYEE
