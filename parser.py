494818ce6f621d66788a67545d0ff5eaec40c114
YANN HAMDI <hamdiyann@gmail.com>
2019-02-08 14:43:02 +0100

    essai paser test au vert

--- parser.py
+++ parser.py
@@ -1,8 +1,10 @@
 """docs function sentence parsing"""
-Class Sentence_parse():
+class SentenceParse:
+        
     def in_lower_case(self, sentence):
 	    self.sentence = str(sentence)   
-	    return self.sentence.lower()
+	    print(self.sentence.lower())
+	    
 #METTRE TOUT EN MINUSCULE
 #SUPPRIMER LES STOPS WORDS
 #RETOURNER PHRASE NETTOYEE
--- test_parsing.py
+++ test_parsing.py
@@ -2,5 +2,5 @@ import parser as script
 
 #test if our function send a sentence in lower case
 def test_in_lower_case():
-	pa = script.Sentence_parse()
+	pa = script.SentenceParse()
 	assert pa.in_lower_case("TEST") == "test"
