a122bf7231934f4a0a52f7f318e22d2539b71ea1
YANN HAMDI <hamdiyann@gmail.com>
2019-02-08 14:45:18 +0100

    passer au vert

--- test_parsing.py
+++ test_parsing.py
@@ -3,4 +3,5 @@ import parser as script
 #test if our function send a sentence in lower case
 def test_in_lower_case():
 	pa = script.SentenceParse()
-	assert pa.in_lower_case("TEST") == "test"
+	x = "TEST"
+	assert pa.in_lower_case(x) == "test"
